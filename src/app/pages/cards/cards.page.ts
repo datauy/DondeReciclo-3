282ecce79ce534469ecee1cbb80796d0b6fec1d8
Fernando Uval <fernandouval@gmail.com>
2020-06-22 01:08:43 -0300

    Se agregan funciones para cambiar el color a los bullets por fuera de las slides tomando el color principal de cada una. Se pone color por defecto como la primer slide ya que la carga es asincrónica

--- src/app/pages/cards/cards.page.ts
+++ src/app/pages/cards/cards.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, QueryList } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ViewChild, ViewChildren } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 import { SessionService } from 'src/app/services/session.service';
 
@@ -10,12 +12,15 @@ import { SessionService } from 'src/app/services/session.service';
 })
 export class CardsPage implements OnInit {
 
+  @ViewChild('cards', {static: false}) slider: IonSlides;
+  @ViewChildren('cards', { read: ElementRef }) slides: QueryList<any>;
   trustedVideoUrl: SafeResourceUrl;
   videos = [{vid_link:"https://www.youtube-nocookie.com/embed/rGlIn4bM9DQ", url:null}];
 
   constructor(
     private domSanitizer: DomSanitizer,
-    public session: SessionService
+    public session: SessionService,
+    private element: ElementRef
   ) { }
 
   /*ionViewWillEnter(): void {
@@ -27,5 +32,23 @@ export class CardsPage implements OnInit {
     for(let i of this.videos){
       i.url = this.domSanitizer.bypassSecurityTrustResourceUrl(i.vid_link);
     }
+  }
+  slideChanging() {
+    this.slider.getActiveIndex().then(
+      index => {
+        this.changeBulletColor(index);
+      }
+    );
+  }
+  private changeBulletColor(index: number) {
+    let bullets = this.element.nativeElement.querySelectorAll('.swiper-pagination-bullet');
+    for (let i in bullets) {
+      if ( bullets[i].nodeName == "SPAN" ) {
+        bullets[i].style.removeProperty('background-color');
+      }
+    }
+    let slide = this.slides.first.nativeElement.children[0].children[index];
+    let bulletActive = this.element.nativeElement.querySelector('.swiper-pagination-bullet-active');
+    bulletActive.style.backgroundColor = getComputedStyle(slide).getPropertyValue('--ion-color-base');
   }
 }
