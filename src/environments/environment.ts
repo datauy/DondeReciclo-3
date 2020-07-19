b5a7237627e104e49d6f4e89bc1ec345dc6ae81b
Daniel Carranza <daniel.chino.carranza@gmail.com>
2020-07-19 19:28:55 -0300

    Assets para backend y ajuste texto menor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   assets: "assets",
-  url: 'http://localhost:3000',
-  backend: "https://dr3b.dev.data.org.uy/api/",
+  url: 'http://localhost:8100',
+  backend: "https://backend.dondereciclo.uy/api/",
   ucenter: [-32.657689, -55.873808]
 };
 
