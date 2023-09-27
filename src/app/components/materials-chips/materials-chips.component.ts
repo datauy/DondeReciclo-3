62ad3dacfe96707316576540d4adeeda37280c95
Fernando Uval <fernandouval@gmail.com>
2023-09-27 08:38:03 -0300

    Se agregan estilos externos y lógica de funcionamiento independente de los chips

--- src/app/components/materials-chips/materials-chips.component.ts
+++ src/app/components/materials-chips/materials-chips.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Material } from "src/app/models/basic_models.model";
 import { ApiService } from "src/app/services/api.service";
 import { SessionService } from 'src/app/services/session.service';
@@ -10,6 +11,7 @@ import { SessionService } from 'src/app/services/session.service';
 })
 export class MaterialsChipsComponent implements OnInit {
   @Input('materials_obj') materials_obj: any;
+  @Input('materials_type') materials_type: string;
   chips: Material[];
 
   constructor(
@@ -24,18 +26,26 @@ export class MaterialsChipsComponent implements OnInit {
     if ( Object.keys(changes['materials_obj'].currentValue).length !== 0 ) {
       let materials_obj = changes['materials_obj'].currentValue;
       this.chips = [];
-      if ( materials_obj.materials != undefined ) {
-        if ( materials_obj.materials.length == 0 ) {
-          this.api.getWastes(materials_obj.wastes).subscribe((wastes) => {
-            this.chips = wastes;
-          });
-        }
-        else {
-          materials_obj.materials.forEach((i) => {
-            this.chips.push(this.api.materials[this.session.country][i]);
-          });
+      if ( this.materials_type == 'search' ) {
+        this.chips = materials_obj;
+      }
+      else {
+        if ( materials_obj.materials != undefined ) {
+          if ( materials_obj.materials.length == 0 ) {
+            this.api.getWastes(materials_obj.wastes).subscribe((wastes) => {
+              this.chips = wastes;
+            });
+          }
+          else {
+            materials_obj.materials.forEach((i) => {
+              this.chips.push(this.api.materials[this.session.country][i]);
+            });
+          }
         }
       }
     }
+  }
+  itemSelected(mat) {
+    this.api._itemSelected.next(mat);
   }
 }
